937a85aec19f4b08ae50feb1fe745e964135953e
Michael Savage <mike@mikejsavage.co.uk>
2025-06-12 16:14:56 +0300

    Fix reset-password

--- src/main.go
+++ src/main.go
@@ -1358,7 +1358,7 @@ func main() {
 			os.Exit( 0 )
 
 		case "reset-password":
-			if len( os.Args ) != 2 {
+			if len( os.Args ) != 3 {
 				showHelpAndQuit()
 			}
 			password := secureRandomHexString( 4 )
