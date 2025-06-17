79efafaa4b3808b1578224f3736653c00d349f4d
Michael Savage <mike@mikejsavage.co.uk>
2025-06-17 23:57:35 +0300

    Remove fuzzysort

--- src/main.go
+++ src/main.go
@@ -53,8 +53,6 @@ var db_schema string
 var alpinejs string
 //go:embed vendor_js/alpinejs-dialog-2.1.1.js
 var alpinejs_dialog string
-//go:embed vendor_js/fuzzysort-3.1.0.js
-var fuzzysortjs string
 //go:embed vendor_js/htmx-2.0.4.js
 var htmxjs string
 //go:embed vendor_js/thumbhash.js
@@ -1514,7 +1512,6 @@ func main() {
 		{ "GET",  "/Special:checksum", getChecksum },
 		{ "GET",  "/Special:alpinejs-3.14.9.js", serveString( alpinejs ) },
 		{ "GET",  "/Special:alpinejs-dialog-2.1.1.js", serveString( alpinejs_dialog ) },
-		{ "GET",  "/Special:fuzzysort-3.1.0.js", serveString( fuzzysortjs ) },
 		{ "GET",  "/Special:htmx-2.0.4.js", serveString( htmxjs ) },
 		{ "GET",  "/Special:thumbhash-1.0.0.js", serveString( thumbhashjs ) },
 
@@ -1547,7 +1544,6 @@ func main() {
 	guest_http_server := startHttpServer( guest_listen_addr, []Route {
 		{ "GET",  "/Special:checksum", getChecksum },
 		{ "GET",  "/Special:alpinejs-3.14.9.js", serveString( alpinejs ) },
-		{ "GET",  "/Special:fuzzysort-3.1.0.js", serveString( fuzzysortjs ) },
 		{ "GET",  "/Special:alpinejs-dialog-2.1.1.js", serveString( alpinejs_dialog ) },
 		{ "GET",  "/Special:htmx-2.0.4.js", serveString( htmxjs ) },
 		{ "GET",  "/Special:thumbhash-1.0.0.js", serveString( thumbhashjs ) },
