c63097efa2072286a5385bbf3c63a4f5b633dd1c
Michael Savage <mike@mikejsavage.co.uk>
2026-05-13 10:21:29 +0300

    Disable PSD/HDR/PIC/PNM in stb_image

--- src/stb/stb_cgo.go
+++ src/stb/stb_cgo.go
@@ -3,6 +3,10 @@ package stb
 /*
 // #cgo LDFLAGS: -lm
 #define STB_IMAGE_IMPLEMENTATION
+#define STBI_NO_PSD
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
 #include "stb_image.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize2.h"
